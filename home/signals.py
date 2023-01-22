cb31fd41bf423a228c4e65e23450b8d661e6badf
ashraf840 <shift2unleashed2011@gmail.com>
2023-01-22 21:42:00 +0600

    CSO-Visitor online connectivity established using channels breathing technique

--- home/signals.py
+++ home/signals.py
@@ -18,6 +18,7 @@ def customer_support_request_signal(sender, instance, created, **kwargs):
         # for sending the payload to each individual cso-channel's frontend wbSocket 
         # through "CSODashboardConsumer".
         channel_layer = get_channel_layer()
+        # TODO: This signal will be custom-made later, logic will be implemented here to decide in which cso-support-dashboard-channel the request will be sent to.
         async_to_sync(channel_layer.group_send)(
             'chat_dashboard',
             {
